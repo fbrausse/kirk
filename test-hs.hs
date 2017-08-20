e78274e019d95c2401e4d82d1f6fe9f7c27abdc7
Franz Brauße <brausse@informatik.uni-trier.de>
2017-08-20 14:12:46 +0900

    haskell: update test-hs to compute sqrt(123.456) and square that using Int and a Real 2

--- test-hs.hs
+++ test-hs.hs
@@ -1,24 +1,33 @@
 
+import Data.Int (Int32)
+import Foreign.C.Types (CDouble(..))
 import Data.Number.Kirk
 import qualified Data.Number.Kirk.Irram as Irram
 
-foreign import ccall "kirk_test_real" kirk_test_real :: Double -> IO KirkRealPtr
+foreign import ccall "kirk-dyadic-real.h" kirk_test_real :: IO KirkRealPtr
+foreign import ccall "kirk-dyadic-real.h" kirk_dyadic_test_real_d :: CDouble -> IO KirkRealPtr
+
+putRealLn :: KirkReal -> Int32 -> IO ()
+putRealLn x acc = do
+  apx <- approx x (AbsAcc acc)
+  putStrLn $ show apx
 
 main :: IO ()
 main = do
   Irram.init
-  kr       <- kirk_test_real 123.456 >>= kirk10
+  two      <- kirk_dyadic_test_real_d 2.0 >>= kirk10
+  putRealLn two (-10)
+  kr       <- kirk_test_real >>= kirk10
   --fr  <- get_real_ptr r
   --apx <- withForeignPtr fr $ \p -> do_real_apx_abs p $ AbsAcc (-10)
   --apx <- approx (KirkReal fr) $ AbsAcc (-10)
   kr_sqrt  <- Irram.sqrt kr
   kr'      <- Irram.pow_n kr_sqrt 2
-  apx      <- approx kr $ AbsAcc (-10)
-  apx_sqrt <- approx kr_sqrt $ AbsAcc (-10)
-  apx'     <- approx kr' $ AbsAcc (-10)
+  kr''     <- Irram.pow   kr_sqrt two
+  putRealLn kr (-10)
+  putRealLn kr_sqrt (-10)
+  putRealLn kr' (-10)
+  putRealLn kr'' (-10)
 --  putStrLn $ "exponent = " ++ (show $ k_exponent $ radius apx)
 --  putStrLn $ "mantissa = " ++ (show $ k_mantissa $ radius apx)
 --  putStrLn $ "center = " ++ (show $ center apx)
-  putStrLn $ show apx
-  putStrLn $ show apx_sqrt
-  putStrLn $ show apx'
