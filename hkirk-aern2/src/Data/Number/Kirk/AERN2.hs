16367fdea1e204e676a66e86078e4f2afa06f0a7
mikkonecny <mikkonecny@gmail.com>
2017-08-20 17:14:39 +0100

    add Michal's Data/Number/Kirk/AERN2.hs from branch aern2-hack

---
+++ hkirk-aern2/src/Data/Number/Kirk/AERN2.hs
@@ -0,0 +1,39 @@
+
+-------------------------------------------------------------------
+-- AERN2 support code
+
+import qualified MixedTypesNumPrelude as MTN
+import System.IO.Unsafe
+import Data.Convertible
+import AERN2.MP.Float
+import AERN2.MP.Dyadic
+import AERN2.Real
+
+instance KirkImportReal CauchyReal where
+  approx r (Effort eff) = approx r (AbsAcc (convert eff))
+  approx r (AbsAcc acc) = 
+    return $ KirkApxT rad c
+    where
+    b = r ? (bitsS $ negate $ toInteger acc)
+    c = mpFloat $ AERN2.Real.centre b
+    radF = mpFloat $ AERN2.Real.radius b
+    (m,e) = decodeFloat radF
+    rad = KirkBoundT (convert e) (convert m)
+
+toAERN2CR :: KirkReal -> CauchyReal
+toAERN2CR r =
+  newCR "kirk" [] makeQ
+    where
+    makeQ (_me, _src) (AccuracySG acS _acG) =
+      updateRadius (MTN.+ (errorBound radF)) (mpBall (dyadic c))
+      where
+      acc = convert (- (fromAccuracy acS))
+      (KirkApxT rad c) = unsafePerformIO (approx r (AbsAcc acc))
+      (KirkBoundT e m) = rad
+      radF 
+          | e >= 0 = encodeFloat (convert m) (convert e) :: MPFloat
+          | otherwise = encodeFloat (convert m * 2^(-e)) 0
+
+-- end of AERN2 support code
+-------------------------------------------------------------------
+
