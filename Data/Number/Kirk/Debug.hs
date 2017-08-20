383c50daf08c53ce6b9270a1a5b9af7968a61542
Franz Brauße <brausse@informatik.uni-trier.de>
2017-08-20 15:39:50 +0900

    haskell: document reason for CDouble in interface

--- Data/Number/Kirk/Debug.hs
+++ Data/Number/Kirk/Debug.hs
@@ -11,5 +11,7 @@ import Foreign.C.Types (CDouble(..))
 
 foreign import ccall "kirk-dyadic-real.h" kirk_dyadic_test_real_d :: CDouble -> IO KirkRealPtr
 
+-- can't provide interface with Double due to realToFrac issues:
+-- <https://ghc.haskell.org/trac/ghc/ticket/3676>
 dyadicTestReal_d :: CDouble -> IO KirkReal
 dyadicTestReal_d x = kirk_dyadic_test_real_d x >>= kirk10
