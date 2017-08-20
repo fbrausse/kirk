83205eb124ba52acdb9d19175af031d1f47bca75
mikkonecny <mikkonecny@gmail.com>
2017-08-20 17:14:39 +0100

    Use AERN2 types in logistic example. Fix error in AERN2 conversion.

--- logmap.hs
+++ logmap.hs
@@ -3,7 +3,8 @@
 
 import Data.Number.Kirk
 import qualified Data.Number.Kirk.Irram as Irram
-import Data.Number.Kirk.Debug
+import qualified Data.Number.Kirk.AERN2 as AERN2
+-- import Data.Number.Kirk.Debug
 import Data.Word
 
 foreign import ccall "" kirk_irram_logmap
@@ -19,8 +20,10 @@ main = do
   let n = 1000
   let p = -300 -- set to -400 to see a crash: hmpfr's limbs can't be realloc'ed
                -- by std mpfr-functions
-  c <- dyadicTestReal_d 3.75
-  x0 <- dyadicTestReal_d 0.5
+--   c <- dyadicTestReal_d 3.75
+--   x0 <- dyadicTestReal_d 0.5
+  let c = AERN2.real (3.75 :: Rational)
+  let x0 = AERN2.real (0.5 :: Rational)
   xn <- logmap n c x0
-  an <- approx xn $ AbsAcc p
-  putStrLn $ show an
+  let xnCR = AERN2.toCR xn
+  putStrLn $ show $ xnCR AERN2.? (AERN2.bitsS (-p))
