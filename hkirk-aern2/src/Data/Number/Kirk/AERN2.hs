c8f3e88e2d70ec009ad4bfd82d95689bb8b7f463
Franz Brauße <brausse@informatik.uni-trier.de>
2017-09-01 14:11:31 +0200

    hkirk-aern2: make AERN2 a module

--- hkirk-aern2/src/Data/Number/Kirk/AERN2.hs
+++ hkirk-aern2/src/Data/Number/Kirk/AERN2.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Data.Number.Kirk.AERN2 where
 
 -------------------------------------------------------------------
 -- AERN2 support code
@@ -5,9 +9,11 @@
 import qualified MixedTypesNumPrelude as MTN
 import System.IO.Unsafe
 import Data.Convertible
-import AERN2.MP.Float
+import AERN2.MP.UseMPFR.Float
 import AERN2.MP.Dyadic
 import AERN2.Real
+
+import Data.Number.Kirk (KirkImportReal(..),KirkSeq0Idx(..),KirkReal,KirkApxT(..),KirkBoundT(..))
 
 instance KirkImportReal CauchyReal where
   approx r (Effort eff) = approx r (AbsAcc (convert eff))
